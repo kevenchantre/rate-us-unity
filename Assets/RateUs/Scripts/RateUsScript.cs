2ef8dccc9126b45ae36142704ad55b649fe1fcb8
kevenchantre <kevenchantre@gmail.com>
2016-11-17 15:30:23 +0100

    Reinventing the wheel. Again.
    This is a Rate Us system for Unity 5.
    It was done using version 5.4 of Unity.

---
+++ Assets/RateUs/Scripts/RateUsScript.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RateUsScript : MonoBehaviour 
+{
+
+	int numBeforeAsking = 0; //How many times the scene have to be loaded before the player is asked to rate the game
+	public string urlToRate = ""; //Url where the player will be redirected to after clicking the Rate Now button
+	public int numBetweenAsking = 0; //How many times the scene have to be loaded before the player is asked to rate the game after he click the button Ask Later.
+
+	void Start () 
+	{
+		//PlayerPrefs.DeleteAll();
+		this.gameObject.SetActive(false);
+		getRateUsState();
+	}
+
+	public static void SetPref (string prefName, int prefValue) 
+	{
+		PlayerPrefs.SetInt(prefName, prefValue);
+	}
+
+	public static int GetPref (string prefName)
+	{
+		return PlayerPrefs.GetInt(prefName);
+	}
+
+	public static int GetPref (string name, int defaultValue)
+	{
+	    if(PlayerPrefs.HasKey(name)) 
+		{
+	        return GetPref(name);
+	    }
+ 
+	    return defaultValue;
+	}
+
+	public bool isRateUsExists
+	{
+		get {return PlayerPrefs.HasKey("rateUs");}
+	}
+
+	public bool isRateUsState
+	{
+		get{return PlayerPrefs.HasKey("isRateUsState");}
+	}
+
+	void getRateUsState ()
+	{
+		if(!isRateUsState)
+		{
+			if(!isRateUsExists)
+			{
+				numBeforeAsking = numBetweenAsking;
+			}
+			else
+			{
+				numBeforeAsking = GetPref("rateUs");
+			}
+			numBeforeAsking = numBeforeAsking - 1;
+			if(numBeforeAsking <= 0)
+			{
+				this.gameObject.SetActive(true);
+			}
+			SetPref("rateUs", numBeforeAsking);
+		}
+	}
+
+	public void RateUs ()
+	{
+		SetPref("isRateUsState", 0);
+		this.gameObject.SetActive(false);
+		Application.OpenURL(urlToRate);
+	}
+
+	public void AskLater ()
+	{
+		numBeforeAsking = numBetweenAsking;
+		SetPref("rateUs", numBeforeAsking);
+		this.gameObject.SetActive(false);
+	}
+
+	public void DontAsk ()
+	{
+		SetPref("isRateUsState", 0);
+		this.gameObject.SetActive(false);
+	}
+}
